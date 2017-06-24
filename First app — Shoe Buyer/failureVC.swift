d0f589a144b29d1f0a63e8c99f8be56eb657becb
RobertBlundell <robert.blundell@club-internet.fr>
2017-06-24 14:51:33 +0200

    everything

---
+++ First app — Shoe Buyer/failureVC.swift
@@ -0,0 +1,35 @@
+//
+//  failureVC.swift
+//  First app — Shoe Buyer
+//
+//  Created by Robert Blundell on 13/05/2017.
+//  Copyright © 2017 Robert Blundell. All rights reserved.
+//
+
+import UIKit
+
+class failureVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ First app — Shoe Buyer/successVC.swift
@@ -0,0 +1,35 @@
+//
+//  successVC.swift
+//  First app — Shoe Buyer
+//
+//  Created by Robert Blundell on 13/05/2017.
+//  Copyright © 2017 Robert Blundell. All rights reserved.
+//
+
+import UIKit
+
+class successVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
